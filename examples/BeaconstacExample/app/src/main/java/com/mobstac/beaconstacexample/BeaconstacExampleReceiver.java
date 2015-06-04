d3dcb9bde299c575245f910590971bae5d19e0b5
Bhashkar Sharma <bhashkarsharma@gmail.com>
2015-06-04 11:35:13 +0530

    Adding BeaconstacExampleReceiver to demonstrate beacon detection when the app is not running

---
+++ examples/BeaconstacExample/app/src/main/java/com/mobstac/beaconstacexample/BeaconstacExampleReceiver.java
@@ -0,0 +1,62 @@
+package com.mobstac.beaconstacexample;
+
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+
+import com.mobstac.beaconstac.core.BeaconstacReceiver;
+import com.mobstac.beaconstac.models.MSBeacon;
+
+import java.util.ArrayList;
+
+/**
+ * Created by bhashkar on 6/4/15.
+ */
+public class BeaconstacExampleReceiver extends BeaconstacReceiver {
+
+    @Override
+    public void exitedBeacon(Context context, MSBeacon beacon) {
+        Log.v(BeaconstacExampleReceiver.class.getName(), "exited called " + beacon.getBeaconKey());
+        sendNotification(context, "Exited " + beacon.getBeaconKey());
+    }
+
+    @Override
+    public void rangedBeacons(Context context, ArrayList<MSBeacon> beacons) {
+        Log.v(BeaconstacExampleReceiver.class.getName(), "Ranged called " + beacons.size());
+        sendNotification(context, "Ranged " + beacons.size() + " beacons");
+    }
+
+    @Override
+    public void campedOnBeacon(Context context, MSBeacon beacon) {
+        Log.v(BeaconstacExampleReceiver.class.getName(), "camped on called " + beacon.getBeaconKey());
+        sendNotification(context, "Camped " + beacon.getBeaconKey());
+    }
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        super.onReceive(context, intent);
+    }
+
+    private void sendNotification(Context context, String text) {
+        if (context != null) {
+            Intent activityIntent = new Intent(context.getApplicationContext(), MainActivity.class);
+            PendingIntent pendingIntent = PendingIntent.getActivity(
+                    context.getApplicationContext(),
+                    0,
+                    activityIntent,
+                    PendingIntent.FLAG_UPDATE_CURRENT
+            );
+            Notification mBuilder = new Notification.Builder(context.getApplicationContext())
+                    .setContentText(text)
+                    .setContentTitle("BeaconstacDemo")
+                    .setSmallIcon(R.mipmap.ic_launcher)
+                    .setContentIntent(pendingIntent).build();
+            NotificationManager notificationManager = (NotificationManager)
+                    context.getApplicationContext().getSystemService(Context.NOTIFICATION_SERVICE);
+            notificationManager.notify(1, mBuilder);
+        }
+    }
+}
